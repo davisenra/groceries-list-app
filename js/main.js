6d06e99cc1b28c016226cee47276cbb9d06130b8
Davi Senra <davi.senra98@gmail.com>
2022-06-03 19:13:19 -0300

    Added checkboxes and new function to submit item with enter

--- js/main.js
+++ js/main.js
@@ -5,25 +5,35 @@ const deleteBtn = document.querySelector("#delete-btn");
 
 const createNewItem = () => {
   const newItem = document.createElement("li");
-  newItem.classList.add(
-    "list-group-item",
-    "d-flex",
-    "gap-2",
-    "justify-content-between"
+  newItem.classList.add("list-group-item", "align-items-center");
+
+  const addCheckBox = document.createElement("input");
+  addCheckBox.classList.add("form-check-input", "me-2", "float-start");
+  addCheckBox.setAttribute("id", "flexCheckDefault");
+  addCheckBox.setAttribute("type", "checkbox");
+  addCheckBox.addEventListener("click", () =>
+    newItem.classList.toggle("checked")
   );
 
   const newCloseBtn = document.createElement("button");
-  newCloseBtn.setAttribute("class", "btn-close");
+  newCloseBtn.classList.add("btn-close", "float-end");
   newCloseBtn.setAttribute("id", "delete-btn");
   newCloseBtn.addEventListener("click", () => newItem.remove());
 
   if (itemName.value != "") {
     newItem.innerText = itemName.value;
+    newItem.appendChild(addCheckBox);
     newItem.appendChild(newCloseBtn);
     itemList.insertBefore(newItem, itemList.firstChild);
     itemName.value = "";
   } else {
     alert("Item field can't be blank");
+  }
+};
+
+addWithEnter = (event) => {
+  if (event.keyCode == 13) {
+    addBtn.click();
   }
 };
 
